28b34434f81cf646d8591d73dbeef487fcc66b5d
vineyardcarmel <carmelvineyard@users.noreply.github.com>
2019-11-13 12:57:33 -0500

    Update rainy_day.py
    Changed to keypress inputs and added a function to close the window. The application is functioning properly now.

--- rainy_day.py
+++ rainy_day.py
@@ -4,7 +4,7 @@ import time
 
 window = turtle.Screen()
 window.bgcolor("maroon")
-window.setup(width=800, height=600)
+window.setup(width=600, height=600)
 window.tracer(0)
 window.title("Rainy Day Dice Roller")
 
@@ -17,44 +17,56 @@ pen.shape("square")
 pen.color("silver")
 pen.penup()
 pen.hideturtle()
-pen.goto(0, 280)
-pen.write("Click a die to roll", align="center", font=("Verdana", 24, "normal"))
+pen.goto(0, 100)
+pen.write("Welcome!\nPress 4 to roll d4\nPress 6 to roll d6\nPress 0 to roll d20", align="center", font=("Verdana", 22, "normal"))
+
+#writing_below
+pen_b = turtle.Turtle()
+pen_b.speed(0)
+pen_b.shape("square")
+pen_b.color("silver")
+pen_b.penup()
+pen_b.hideturtle()
+pen_b.goto(0, -200)
+pen_b.write("Press c to clear, press [space] to close", align="center", font=("Verdana", 22, "normal"))
+
 
 #Label_4
 pen4 = turtle.Turtle()
 pen4.speed(0)
 pen4.shape("square")
-pen4.color("maroon")
+pen4.color("yellow")
 pen4.penup()
 pen4.hideturtle()
-pen4.goto(-200, 0)
-pen4.write("D4", align="center", font=("Verdana", 24, "normal"))
+pen4.goto(-200, -75)
+pen4.write("D4", align="center", font=("Verdana", 20, "normal"))
 
 #Label_6
 pen6 = turtle.Turtle()
 pen6.speed(0)
 pen6.shape("square")
-pen6.color("maroon")
+pen6.color("yellow")
 pen6.penup()
 pen6.hideturtle()
-pen6.goto(0, 0)
-pen6.write("D6", align="center", font=("Verdana", 24, "normal"))
+pen6.goto(0, -75)
+pen6.write("D6", align="center", font=("Verdana", 20, "normal"))
 
 #Label_20
 pen20 = turtle.Turtle()
 pen20.speed(0)
 pen20.shape("square")
-pen20.color("maroon")
+pen20.color("yellow")
 pen20.penup()
 pen20.hideturtle()
-pen20.goto(200, 0)
-pen20.write("D20", align="center", font=("Verdana", 24, "normal"))
+pen20.goto(200, -75)
+pen20.write("D20", align="center", font=("Verdana", 20, "normal"))
 
 #The Dice
 #D4
 d4 = turtle.Turtle()
 d4.speed(0)
 d4.shape("triangle")
+d4.left(90)
 d4.color("white")
 d4.shapesize(stretch_wid=2,stretch_len=2)
 d4.penup()
@@ -81,39 +93,49 @@ d20.penup()
 d20.goto(200, 0)
 
 
+#Generating the random dice rolls
+
+def roll_d4():
+    rand_d4 = random.randint(1, 4)
+    time.sleep(.5)
+    pen4.clear()
+    pen4.write(rand_d4, align="center", font=("Garamond", 20, "normal"))
+    return rand_d4
+
+def roll_d6():
+    rand_d6 = random.randint(1, 6)
+    time.sleep(.5)
+    pen6.clear()
+    pen6.write(rand_d6, align="center", font=("Garamond", 20, "normal"))
+    return rand_d6
+
+def roll_d20():
+    rand_d20 = random.randint(1, 20)
+    time.sleep(.5)
+    pen20.clear()
+    pen20.write(rand_d20, align="center", font=("Garamond", 20, "normal"))
+    return rand_d20
+
+def reset():
+    pen4.clear()
+    pen6.clear()
+    pen20.clear()
+    pen4.write("D4", align="center", font=("Verdana", 20, "normal"))
+    pen6.write("D6", align="center", font=("Verdana", 20, "normal"))
+    pen20.write("D20", align="center", font=("Verdana", 20, "normal"))
+
+def bye_bye():
+    window.bye()
+
 #Keyboard bindings
 window.listen()
 window.onkeypress(roll_d4, "4")
 window.onkeypress(roll_d6, "6")
 window.onkeypress(roll_d20, "0")
 window.onkeypress(reset, "c")
+window.onkeypress(bye_bye, "space")
 
-
-
-#Generating the random dice rolls
-
-def roll_d4():
-    rand_d4 = random.randint(1, 4)
-    time.sleep(2)
-    return rand_d4
-
-def roll_d6():
-    rand_d6 = random.randint(1, 6)
-    time.sleep(2)
-    return rand_d4
-
-def roll_d20():
-    rand_d20 = random.randint(1, 20)
-    time.sleep(2)
-    return rand_d20
-
-def reset():
-    pen4.write("D4", align="center", font=("Verdana", 24, "normal"))
-    pen6.write("D6", align="center", font=("Verdana", 24, "normal"))
-    pen20.write("D20", align="center", font=("Verdana", 24, "normal"))
-
-
-
-
-
-
+#Main game loop
+while True:
+    window.update()
+    
